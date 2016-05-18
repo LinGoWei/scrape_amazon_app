23f658d06d94dd756fcadf0723e3dc52e27202d0
root <root@master1.(none)>
2016-05-18 13:15:23 +0800

    multi process scrape

--- src/scrape.py
+++ src/scrape.py
@@ -10,6 +10,7 @@ import redis
 from multiprocessing import Process
 from requests.packages.urllib3.util.retry import Retry
 from requests.adapters import HTTPAdapter
+from copy import deepcopy
 
 from constant import user_agents
 from scrape_proxies import ProxyService
@@ -17,8 +18,8 @@ from utils import get_logger, get_connection, retry, load_ids_set
 from constant import APP_SOURCE_PAGE_KEY 
 
 AMAZON_APP_URL = 'http://www.amazon.com/dp/{app_id}'
-APP_PAGE_SIZE = 10000
-
+REJECT_PAGE_SIZE = 10000 # 10K
+NORMAL_APP_PAGE_SIZE = 100000 # 100K
 
 logger = get_logger('scrap.log', __name__)
 
@@ -30,60 +31,64 @@ class AmazonAppSpider(object):
 
     def scrape(self, date_str, app_ids_list):
         thread.start_new_thread(self._load_proxies, (600, ))
-
-        for app_id in app_ids_list:
+        
+	for app_id in app_ids_list:
             app_page_key = APP_SOURCE_PAGE_KEY.format(date=date_str, app_id=app_id)
 	    if self.redis_obj.exists(app_page_key):
                 continue
             try:
                 self._scrape(app_id, app_page_key)
-            except Exception as ex:
+	    except Exception as ex:
                 print 'Failed scrape and save app', app_id
 		logger.exception(ex)
 		logger.info('Failed scrape and save app {}'.format(app_id))
 
-    @retry(3)
+    @retry(2)
     def _scrape(self, app_id, app_page_key):
         scrape_url = AMAZON_APP_URL.format(app_id=app_id)
         header = {'content-type': 'text/html',
                   'User-Agent': user_agents[random.randint(0, len(user_agents)-1)]}
         proxy = self.proxy_service.get_proxy()
-        try:
+	try:
 	    req = requests.Session()
 	    retries = Retry(total=2, backoff_factor=0.1, status_forcelist=[500, 502, 503, 504])
 	    req.mount('http://', HTTPAdapter(max_retries=retries))
 	    response = req.get(scrape_url, timeout=80, headers=header, proxies=proxy)
-	    if len(response.content) > APP_PAGE_SIZE:
-                print 'Succeed scrape app', app_id
-		self._save_page(app_page_key, response.content)
-                self.proxy_service.manage(proxy, False)
-           	print 'Succeed save redis app', app_id
-		logger.info('Succeed scrape and save app {}'.format(app_id))
+	    if len(response.content) > REJECT_PAGE_SIZE:
+	        if len(response.content) > NORMAL_APP_PAGE_SIZE:
+    		    print 'Succeed scrape app', app_id
+		    self._save_page(app_page_key, response.content)
+                    self.proxy_service.manage(proxy, False)
+           	    print 'Succeed save redis app', app_id
+		    logger.info('Succeed scrape and save app {}'.format(app_id))
+	        else:
+           	    print 'Invalid app', app_id
+		    logger.info('Invalid app {}'.format(app_id))
 	    else:
                 raise Exception('Failed visit app page')
 	
         except Exception as ex:
-	    logger.exception(ex)
             self.proxy_service.manage(proxy, True)
-            raise Exception('Failed scrape app page')
+            raise ex 
 
     def _save_page(self, app_page_key, content):
         self.redis_obj.set(app_page_key, content)
 
     def _load_proxies(self, delay):
-        while True:
-            self.proxy_service.load_proxies('proxies.csv')
+        logger.info('Start thread to load proxy.')
+	while True:
+	    self.proxy_service.load_proxies('proxies.csv')
 	    size = self.proxy_service.get_valid_size()
-            print 'Succeed load proxies. Valid size:', size
-	    logger.info('Succeed load proxies')
-            time.sleep(delay)
+	    print 'Succeed load proxies. Valid size:', size
+	    logger.info('Succeed load proxies. Valid size {}'.format(size))
+	    time.sleep(delay)
 
 
 def multi_scrape(i, date, ids):
     print 'Start process', i
     logger.info('Start process {}, need to scrape {} apps'.format(i, len(ids)))
     amazon_app_spider = AmazonAppSpider()
-    amazon_app_spider.scrape(args.date, app_ids_list)
+    amazon_app_spider.scrape(date, ids)
     print 'Succeed finish process', i
     logger.info('Succeed finish process {}'.format(i))
 
@@ -111,8 +116,8 @@ if __name__ == '__main__':
 
     process_list = list()
     for i in range(0, args.batch):
-	ids = app_ids_list[i * batch_size: (i+1) * batch_size]
-	process_list.append(Process(target=multi_scrape, args=(i, args.date, ids)))
+	app_ids = app_ids_list[i * batch_size: (i+1) * batch_size]
+	process_list.append(Process(target=multi_scrape, args=(i, args.date, app_ids)))
 	process_list[-1].start()
 
     for p in process_list:
