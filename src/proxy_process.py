7470b7e750fbf1c697585e41fa2855d450125cc0
root <root@master1.(none)>
2016-05-19 12:37:59 +0800

    modify

---
+++ src/proxy_process.py
@@ -0,0 +1,11 @@
+import time
+
+from services.proxy_service import ProxyService
+
+
+if __name__ == '__main__':
+    proxy_service = ProxyService()
+    while True: 
+        proxy_service.process()
+        print 'Succeed scrape proxy.'
+        time.sleep(60 * 10)
