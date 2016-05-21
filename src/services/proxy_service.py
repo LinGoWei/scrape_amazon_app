85c7ac9616e823b9c4169331fcbf0e04482a92ab
root <root@master1.(none)>
2016-05-21 11:19:38 +0800

    nice to scrape amazon

--- src/services/proxy_service.py
+++ src/services/proxy_service.py
@@ -4,7 +4,7 @@ import requests
 from bs4 import BeautifulSoup
 
 from constant import user_agents
-from utils import retry
+from utils import retry, get_logger
 from services.redis_service import RedisService
 
 __author__ = 'Blyde'
@@ -12,36 +12,26 @@ __author__ = 'Blyde'
 PROXY_URL = '{protocol}://{ip}:{port}'
 PROXY_URL_KEY = 'proxy-urls'
 
-BACKUP_PROXY_URL_SET = {
-    'http://182.120.31.119:9999',
-    'http://183.62.206.210:3128',
-    'http://60.191.165.100:3128',
-    'http://106.7.154.67:9000',
-}
 DEFAULT_ERROR_TIMES = 1
+logger = get_logger(__name__)
 
 
 class ProxyService(object):
     def __init__(self):
         self.redis_service = RedisService()
-        self.proxy_url_set = BACKUP_PROXY_URL_SET
         self.error_proxy_dict = dict()
-        self.invalid_proxy_url_set = set()
 
     def get_proxy(self):
-        proxy = list(self.proxy_url_set)[random.randint(0, len(self.proxy_url_set) - 1)]
+        proxy = self.redis_service.read_set(PROXY_URL_KEY)
         if not proxy:
             return None
         return {'http': proxy}
 
     def get_valid_size(self):
-        return len(self.proxy_url_set)
-
-    def load_proxies(self):
-        proxy_urls = self.redis_service.pull_list(PROXY_URL_KEY)
-        self.proxy_url_set.update(set(proxy_urls) - self.invalid_proxy_url_set)
+        return self.redis_service.get_set_size(PROXY_URL_KEY)
 
     def process(self):
+        logger.info('Start load proxy.')
         content = self._scrape()
         parser_proxy_url_set = self._parser(content)
         self._save(parser_proxy_url_set)
@@ -54,8 +44,9 @@ class ProxyService(object):
             if proxy_url in self.error_proxy_dict:
                 self.error_proxy_dict[proxy_url] += 1
                 if self.error_proxy_dict[proxy_url] > DEFAULT_ERROR_TIMES:
-                    self.proxy_url_set.remove(proxy_url)
+                    self.redis_service.pop_set(PROXY_URL_KEY, proxy_url)
                     self.error_proxy_dict.pop(proxy_url)
+                    logger.info('Invalid proxy: {}'.format(proxy_url))
             else:
                 self.error_proxy_dict[proxy_url] = 1
         else:
@@ -86,4 +77,5 @@ class ProxyService(object):
         return parser_proxy_url_set
 
     def _save(self, parser_proxy_url_set):
-        self.redis_service.push_list(PROXY_URL_KEY, parser_proxy_url_set)
+        for url in parser_proxy_url_set:
+            self.redis_service.add_set(PROXY_URL_KEY, url)
