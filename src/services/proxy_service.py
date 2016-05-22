92bce9eb24b7c20864bbac58511a2db3fbc94410
lingowei <weiliu32992016@qq.com>
2016-05-22 23:16:31 +0800

    https proxy

--- src/services/proxy_service.py
+++ src/services/proxy_service.py
@@ -10,7 +10,7 @@ from services.redis_service import RedisService
 __author__ = 'Blyde'
 
 PROXY_URL = '{protocol}://{ip}:{port}'
-PROXY_URL_KEY = 'proxy-urls'
+PROXY_URL_KEY = '{protocol}-proxy-urls'
 
 DEFAULT_ERROR_TIMES = 1
 logger = get_logger(__name__)
@@ -21,20 +21,25 @@ class ProxyService(object):
         self.redis_service = RedisService()
         self.error_proxy_dict = dict()
 
-    def get_proxy(self):
-        proxy = self.redis_service.read_set(PROXY_URL_KEY)
+    def get_proxy(self, protocol):
+        proxy = self.redis_service.read_set(PROXY_URL_KEY.format(protocol))
         if not proxy:
             return None
-        return {'http': proxy}
+        return {protocol: proxy}
 
-    def get_valid_size(self):
-        return self.redis_service.get_set_size(PROXY_URL_KEY)
+    def get_valid_size(self, protocol):
+        return self.redis_service.get_set_size(PROXY_URL_KEY.format(protocol))
 
     def process(self):
         logger.info('Start load proxy.')
-        content = self._scrape()
-        parser_proxy_url_set = self._parser(content)
-        self._save(parser_proxy_url_set)
+        content = self._scrape_http_proxy()
+        parser_proxy_url_set = self._parser_http_proxy(content)
+        self._save('http', parser_proxy_url_set)
+
+        content = self._scrape_https_proxy()
+        parser_proxy_url_set = self._parser_https_proxy(content)
+        print parser_proxy_url_set
+        self._save('https', parser_proxy_url_set)
 
     def manage(self, proxy, error):
         if not proxy:
@@ -54,7 +59,7 @@ class ProxyService(object):
                 self.error_proxy_dict[proxy_url] -= 1
 
     @retry(2)
-    def _scrape(self):
+    def _scrape_http_proxy(self):
         scrape_url = 'http://www.xicidaili.com/nn'
         header = {'content-type': 'text/html',
                   'User-Agent': user_agents[random.randint(0, len(user_agents)-1)]}
@@ -65,7 +70,17 @@ class ProxyService(object):
             raise Exception('Failed scrape proxies.')
 
     @retry(2)
-    def _parser(self, content):
+    def _scrape_https_proxy(self):
+        scrape_url = 'http://www.nianshao.me/?stype=2'
+        header = {'content-type': 'text/html',
+                  'User-Agent': user_agents[random.randint(0, len(user_agents)-1)]}
+        try:
+            response = requests.get(scrape_url, headers=header, proxies=None)
+            return response.content
+        except:
+            raise Exception('Failed scrape proxies.')
+
+    def _parser_http_proxy(self, content):
         soup = BeautifulSoup(content, 'html.parser')
         proxy_tag = soup.find(id='ip_list').select('tr')
         parser_proxy_url_set = set()
@@ -76,6 +91,17 @@ class ProxyService(object):
             parser_proxy_url_set.add(proxy_url)
         return parser_proxy_url_set
 
-    def _save(self, parser_proxy_url_set):
+    def _parser_https_proxy(self, content):
+        soup = BeautifulSoup(content, 'html.parser')
+        proxy_tag = soup.find_all('tr')
+        parser_proxy_url_set = set()
+        for i in range(1, 16):
+            proxy_url = PROXY_URL.format(protocol='https',
+                                         ip=proxy_tag[i].find_all('td')[0].string,
+                                         port=proxy_tag[i].find_all('td')[1].string)
+            parser_proxy_url_set.add(proxy_url)
+        return parser_proxy_url_set
+
+    def _save(self, protocol, parser_proxy_url_set):
         for url in parser_proxy_url_set:
-            self.redis_service.add_set(PROXY_URL_KEY, url)
+            self.redis_service.add_set(PROXY_URL_KEY.format(protocol), url)
